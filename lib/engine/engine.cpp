f8d307c213e9cc3368d4ade3d5cfd6ef23e590c6
Liam <60526247+The-Liam-Blair@users.noreply.github.com>
2022-04-04 16:59:34 +0100

    Modified custom loading scene
    - Added moving red progress bar at the bottom of the screen.
    - Modified loading text to pulse by changing it's opacity.
    - Moved object creation of text and loading bar to be global variables so they do not need to be instantiated per frame (Only once now).
    - Added internal commentary of relevant and changed variables/functions.

--- lib/engine/engine.cpp
+++ lib/engine/engine.cpp
@@ -13,33 +13,58 @@ using namespace std;
 Scene* Engine::_activeScene = nullptr;
 std::string Engine::_gameName;
 
-static bool loading = false;
-static float loadingspinner = 0.f;
-static float loadingTime;
-static RenderWindow* _window;
+static bool loading = false; // Loading new scene
+static float loadTextOpacity = 0.f; // Rapidly increments/decrement between 50 and 250, sets opacity of loading text.
+static float loadingTime = 0.f; // Time taken so far to load the next scene.
+static bool gainingOpacity = true; // Bool that tracks if the opacity of the loading text should be incrementing or decrementing.
 
+static RectangleShape loadingBar(Vector2f(100, 10)); // Create the bar (Doing this outside the render function to prevent initializing this object per frame.
+static Text t("L O A D I N G . . .", *Resources::get<sf::Font>("RobotoMono-Regular.ttf")); // Loading text object with pre-defined font.
+
+static RenderWindow* _window; // Game window.
+
+/**
+ * \brief Loading menu update function where the loading text's opacity will cycle and check if the currently-loading scene has fully loaded.
+ * \param dt Delta time.
+ * \param scn Scene being loaded.
+ */
 void Loading_update(float dt, const Scene* const scn) {
-  //  cout << "Eng: Loading Screen\n";
-  if (scn->isLoaded()) {
+
+    // Stop loading if scene is fully loaded.
+  if (scn->isLoaded()) 
+  {
     cout << "Eng: Exiting Loading Screen\n";
     loading = false;
-  } else {
-    loadingspinner += 220.0f * dt;
-    loadingTime += dt;
+  }
+    // If scene is not loaded, calculate new opacity of the loading text, increment the loading time to be used in loading screen render.
+  else 
+  {
+  	loadTextOpacity += (gainingOpacity) ? 300 * dt : -300 * dt;
+  	if (loadTextOpacity < 50) { gainingOpacity = true; }
+  	else if (loadTextOpacity > 200) { gainingOpacity = false; }
+  	loadingTime += dt;
   }
 }
+
+/**
+ * \brief Render a red bar at the bottom of the screen, visually moving to represent loading progress.
+ * \n Also, create a text that says "Loading..." which slowly flashes to show that the program is running and loading the next level.
+ */
 void Loading_render() {
-  // cout << "Eng: Loading Screen Render\n";
-  static CircleShape octagon(80, 8);
-  octagon.setOrigin(Vector2f(80, 80));
-  octagon.setRotation(sf::degrees(loadingspinner));
-  octagon.setPosition(Vcast<float>(Engine::getWindowSize()) * .5f);
-  octagon.setFillColor(Color(255,255,255,min(255.f,40.f*loadingTime)));
-  static Text t("Loading", *Resources::get<sf::Font>("RobotoMono-Regular.ttf"));
-  t.setFillColor(Color(255,255,255,min(255.f,40.f*loadingTime)));
-  t.setPosition(Vcast<float>(Engine::getWindowSize()) * Vector2f(0.4f,0.3f));
-  Renderer::queue(&t);
-  Renderer::queue(&octagon);
+    // Set loading bar position, scale it equal to 10 * loading time (Bar moves to the right, acting as a loading bar).
+	loadingBar.setPosition(Vector2f(0, Engine::getWindowSize().y));
+    loadingBar.setOrigin(Vector2f(50, 10));
+    loadingBar.setScale(Vector2f(10 * loadingTime, 1));
+	loadingBar.setFillColor(Color::Red);
+
+    // Set text fill colour, character size and position.
+	t.setFillColor(Color(255,255,255, loadTextOpacity));
+	t.setCharacterSize(40);
+	t.setPosition(Vector2f(Engine::getWindowSize().x * 0.3, Engine::getWindowSize().y * 0.7));
+
+    // Queue the loading text and bar to the system renderer.
+	Renderer::queue(&t);
+	Renderer::queue(&loadingBar);
 }
 
 float frametimes[256] = {};
@@ -132,8 +157,7 @@ void Engine::ChangeScene(Scene* s) {
   if (!s->isLoaded()) {
     cout << "Eng: Entering Loading Screen\n";
     loadingTime =0;
-    //_activeScene->LoadAsync();
-    _activeScene->Load();
+    _activeScene->LoadAsync();
     loading = true;
   }
 }
