20d4e529654ff0ae59cd7c69903a1a1bd144b23a
Liam <40437578@live.napier.ac.uk>
2022-04-22 20:49:28 +0100

    Finalised Game Over Scene Flow
    - Instead of waiting for user input, the game over scene will automatically transition back to the main menu after a short time delay, as using same keyboard inputs will trigger the scene change to the main menu and then trigger another event waiting on user input in the main menu accidentally.

--- maze_escape/scenes/scene_lose.cpp
+++ maze_escape/scenes/scene_lose.cpp
@@ -1,25 +1,23 @@
 #include "scene_lose.h"
 #include "../components/cmp_text.h"
 #include "../game.h"
-#include <SFML/Window/Keyboard.hpp>
-#include <iostream>
 
 using namespace std;
 using namespace sf;
 
-void LoseScene::Load() {
-    cout << "Menu Load \n";
-    {
+float timer = 5.0f;
+void LoseScene::Load()
+{
         auto txt = makeEntity();
-        auto t = txt->addComponent<TextComponent>(
-            "You Lose.\n\nPress Space to return to the main menu.");
-    }
+        auto text = txt->addComponent<TextComponent>("You lose.\nReturning to the main menu in 5 seconds...");
     setLoaded(true);
 }
 
 void LoseScene::Update(const double& dt)
 {
-    if (sf::Keyboard::isKeyPressed(Keyboard::Space)) {
+    timer -= dt;
+    if (timer <= 0) 
+    {
         Engine::ChangeScene(&menu);
     }
 
